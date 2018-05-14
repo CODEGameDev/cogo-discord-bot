e155fdfea5d2e7542a3601dff48932b5cb309177
Darian <thehelljelp@gmail.com>
2018-05-14 12:39:10 +0200

    add requirements.txt

---
+++ cogo/foobar.py
@@ -0,0 +1,3 @@
+def hello_word():
+    return "world"
+    
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import cogo.foobar
+
 def func():
     return "hello"
 
 def test_answer():
     assert func() == "hello"
+
+def test_main():
+    assert cogo.foobar.hello_word() == "world"
