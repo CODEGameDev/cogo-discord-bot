18e598e95623fa72df4775406aed5c415d441ab7
Darian <thehelljelp@gmail.com>
2018-05-14 14:15:40 +0200

    fix build error

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -8,12 +8,16 @@ should return 100%
 
 import sys
 import os
-import cogo.foobar
-
-
 MY_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, MY_PATH + '/../')
+
+import cogo.foobar #pylint: disable=C0413
+
 
 def test_main():
     """tests placeholder method"""
     assert cogo.foobar.hello_word() == "world"
+
+
+if __name__ == '__main__':
+    test_main()
